a365d9a4207bd460e4f82e869bc939626a258e92
ROBODEVIL857 <55064835+ROBODEVIL857@users.noreply.github.com>
2021-03-04 11:17:18 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+
+var Config = {
+    WinStreak: 5,
+    Wide: 15,
+    Tall: 15,
+    BoxOffset: {
+        x:1,
+        y:1
+    },
+    Playername1: "Player 1",
+    Playername2: "Player 2"
+}
+
+module.exports = Config
